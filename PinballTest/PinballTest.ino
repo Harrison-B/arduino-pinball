74366528c6911ca0fb59f5ac3868c7d13f622be1
Harrison Bolin <harrisonbolin@gmail.com>
2019-11-14 11:23:19 -0700

    Create PinballTest.ino

---
+++ PinballTest/PinballTest.ino
@@ -0,0 +1,20 @@
+// Teired & timed obstacle
+const int teiredSensorOne = 13;
+const int teiredSensorTwo = 11;
+const int teiredSensorThree = 10;
+
+const int teiredLedOne = 8;
+
+// Scoring variables
+int totalScore = 0;
+
+void setup() {
+  pinMode(teiredSensorOne, INPUT);
+  pinMode(teiredSensorTwo, INPUT);
+  
+  pinMode(teiredLedOne, OUTPUT);
+}
+
+void loop() {
+  
+}
